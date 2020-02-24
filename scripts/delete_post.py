83dc6907bfd6ba4cedfe5d1853e98e4e6279f13b
benkalat <bentsikalat@gmail.com>
2020-02-24 10:47:46 +0200

    24/02

--- scripts/delete_post.py
+++ scripts/delete_post.py
@@ -32,9 +32,7 @@ try:
 
 
 except BaseException as e:
-    print("location: ../change_password.html?err=1&msg="+str(e))
-    print("")
-    print('Failed to do something: ' + str(e))
+    print(e)
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(exc_type, fname, exc_tb.tb_lineno)
